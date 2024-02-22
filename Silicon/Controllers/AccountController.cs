4c9aa91c9500a410daf861f5e9527e038fbdbb86
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-02-22 12:40:59 +0100

    account details init

---
+++ Silicon/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
