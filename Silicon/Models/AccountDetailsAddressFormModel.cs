12ad5d6913a7fab1380b40d462404d98068c94c0
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-02-22 16:21:59 +0100

    Account details finished

---
+++ Silicon/Models/AccountDetailsAddressFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Silicon.Models;
+
+public class AccountDetailsAddressFormModel
+{
+    [Display(Name = "Address Line 1", Prompt = "Enter your address line", Order = 0)]
+    [Required(ErrorMessage = "Address line 1 is required")]
+    public string AddressLine1 { get; set; } = null!;
+
+    [Display(Name = "Address Line 2", Prompt = "Enter your secondary address line", Order = 1)]
+    public string? AddressLine2 { get; set; }
+
+    [Display(Name = "Postal Code", Prompt = "Enter your postal code", Order = 2)]
+    [Required(ErrorMessage = "Postal code is required")]
+    [DataType(DataType.PostalCode)]
+    public string PostalCode { get; set; } = null!;
+
+    [Display(Name = "City", Prompt = "Enter your city", Order = 3)]
+    [Required(ErrorMessage = "City is required")]
+    public string City { get; set; } = null!;
+}
---
+++ Silicon/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Silicon.Models;
+
+namespace Silicon.ViewModels;
+
+public class AccountDetailsViewModel
+{
+    public string Title { get; set; } = "Account Details";
+
+    public AccountDetailsBasicFormModel BasicForm { get; set; } = new AccountDetailsBasicFormModel
+    {
+        FirstName = "John",
+        LastName = "Doe",
+        Email = "john.doe@domain.com",
+        ProfileImage = "images/profile-image.svg"
+    };
+    public AccountDetailsAddressFormModel AddressForm { get; set; } = new AccountDetailsAddressFormModel();
+}
