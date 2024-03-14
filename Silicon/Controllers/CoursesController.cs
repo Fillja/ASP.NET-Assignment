63f927223107224f670fa07cdf774f5f2b259c89
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-03-14 13:17:33 +0100

    Courses init (Design, Controller, View & Partial).

---
+++ Silicon/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+[Authorize]
+public class CoursesController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Courses";
+        return View();
+    }
+
+    public IActionResult SingleCourse()
+    {
+        return View();
+    }
+
+    public IActionResult Bookmark()
+    {
+        return View();
+    }
+}
