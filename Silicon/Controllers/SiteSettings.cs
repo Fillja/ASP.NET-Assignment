0570dff4dd06919c034868163c4c3957c7587038
Robin Gombkoto <robin.gombkoto@gmail.com>
2024-04-01 12:29:35 +0200

    Dark mode implemented and completed.

---
+++ Silicon/Controllers/SiteSettings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+public class SiteSettings : Controller
+{
+    public IActionResult ChangeTheme(string theme)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(60),
+        };
+        Response.Cookies.Append("ThemeMode", theme, option);
+        return Ok();
+    }
+}
--- Silicon/wwwroot/js/site.js
+++ Silicon/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    let sw = document.querySelector('#switch-mode')
+
+    sw.addEventListener('change', function () {
+        let theme = this.checked ? "dark" : "light"
+
+        fetch(`/sitesettings/changetheme?theme=${theme}`)
+            .then(res => {
+                if (res.ok)
+                    window.location.reload()
+                else 
+                    console.log('Something went wrong')
+            })
+    })
+})
